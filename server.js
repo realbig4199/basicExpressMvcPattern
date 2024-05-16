91b1d97f763c4eb96a40efb1124476aeb1907d3f
realbig4199 <realbig4199@gmail.com>
2024-05-16 21:12:56 +0900

    환경 변수 추가

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose"); // mongoose 임포트
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 // 라우터 불러오기
 const usersRouter = require("./routes/users.router");
 const postsRouter = require("./routes/posts.router");
@@ -12,8 +13,7 @@ const app = express();
 // const mongoURI = "mongodb://localhost:27017/mydatabase";
 // 몽고DB Atlas 연결 URL
 // 패스워드는 설정한 패스워드로 변경 (실제로는 .env 파일에 저장하여 사용하는 게 보안상 좋음)
-const mongoURI =
-  "mongodb+srv://realbig419:0419@cluster0.3ahcwzm.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0";
+const mongoURI = process.env.MONGO_URI;
 
 // Mongoose를 사용하여 MongoDB에 연결
 mongoose
